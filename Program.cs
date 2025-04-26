ee03d58b5d1985813547485d21a2c6cba3b0f469
Kiritmak <iran07ma@gmail.com>
2025-04-26 14:52:58 -0400

    Implemented a welcome message and Input/Output library for reading integers in an interval and also for reading any variable

--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Program
+{
+  public class IO
+  {
+    public static T ReadInput<T>() where T : IParsable<T>
+    {
+      T variable;
+      while(!T.TryParse(Console.ReadLine(), null, out variable))
+      {
+        Console.WriteLine("Incorrect Format");
+      }
+      return variable;
+    }
+    public static int ReadIntInRange(int min=int.MinValue, int mx = int.MaxValue)
+    {
+      int variable = IO.ReadInput<int>();
+      while(variable < min || variable > mx)
+      {
+        Console.WriteLine("Incorrect Format");
+        variable = IO.ReadInput<int>();
+      }
+      return variable;
+    }
+  }
+  public class ATM
+  {
+    public static void Main(String[] args)
+    {
+      string[] opciones = ["Close", "Deposit", "Extract", "Transfeer"];
+      int opcion = -1;
+      do
+      {
+        Console.WriteLine("Welcome to the ATM, please select an action to do:");
+        for (int i = 0; i < opciones.Length; i++)
+          Console.WriteLine("{0} => {1}", i, opciones[i]);
+        opcion = IO.ReadIntInRange(0, opciones.Length - 1);
+      } while (opcion != 0);
+
+      
+    }
+  }
+}
+
+
